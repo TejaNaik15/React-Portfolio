f3d797020d5ff7d3e557075b16583ff557d99e3a
TEJANAIK <134024083+TejaNaik15@users.noreply.github.com>
2025-09-17 23:44:26 +0530

    Updated photo in  ProfileCard.jsx

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -20,7 +20,7 @@ const adjust = (value, fromMin, fromMax, toMin, toMax) =>
 const easeInOutCubic = (x) => (x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2);
 
 const ProfileCardComponent = ({
-  avatarUrl = '/assets/naik.jpg',
+  avatarUrl = '/assets/TEJANAIK.png',
   iconUrl = '',
   grainUrl = '',
   behindGradient,
